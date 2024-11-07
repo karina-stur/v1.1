3d9d8ea880e888ab57969a1f42dd405528e3e192
karina-stur <sturonaite.k@gmail.com>
2024-11-07 19:08:59 +0200

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -1,14 +1,32 @@
 #ifndef FUNKCIJOS_H
 #define FUNKCIJOS_H
 
+#include <iostream>
+#include <iomanip>
 #include <vector>
+#include <list>
 #include <string>
+#include <fstream>
+#include <random>
+#include <algorithm>
+#include <numeric>
+#include <chrono>
+#include <limits>
 #include "studentas.h"
 
-void nuskaititiIsFailo(const std::string& failoPavadinimas, std::vector<Studentas>& studentai);
-double skaiciuotiGalutiniVidurki(const std::vector<int>& namuDarbai, int egzaminas);
-double skaiciuotiGalutiniMediana(std::vector<int> namuDarbai, int egzaminas);
-void surikiuotiStudentus(std::vector<Studentas>& studentai);
+void generuotiStudentuFailus();
+void generuotiPazymius(Studentas& studentas, int namuDarbuKiekis);
+void nuskaitytiStudentus(const std::string& failoPavadinimas, std::vector<Studentas>& studentai);
+void nuskaitytiStudentus(const std::string& failoPavadinimas, std::list<Studentas>& studentai);
+void grupuotiIrIssaugotiStudentus(std::vector<Studentas>& studentai, bool pagalVidurki);
+void grupuotiIrIssaugotiStudentus(std::list<Studentas>& studentai, bool pagalVidurki);
+void isaugotiStudentuGrupe(const std::vector<Studentas>& studentai, const std::string& failoPavadinimas, bool pagalVidurki);
+void isaugotiStudentuGrupe(const std::list<Studentas>& studentai, const std::string& failoPavadinimas, bool pagalVidurki);
+void ivestiStudentuDuomenis(std::vector<Studentas>& studentaiVektorius, std::list<Studentas>& studentaiSarasas);
+int readInteger();
+
+void testKonteinerius(const std::vector<Studentas>& studentaiVektorius, const std::list<Studentas>& studentaiSarasas, bool pagalVidurki,
+    double& totalPartitionTimeVec, double& totalNuskriaustukaiSaveTimeVec, double& totalKietiakaiSaveTimeVec, double& readingTimeVec,
+    double& totalReadingTimeList, double& totalPartitionTimeList, double& totalNuskriaustukaiSaveTimeList, double& totalKietiakaiSaveTimeList);
 
 #endif // FUNKCIJOS_H
-
