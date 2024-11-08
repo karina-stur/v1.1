5cc646151713cd77abd218eec988fc80bfbe89c2
karina-stur <sturonaite.k@gmail.com>
2024-11-08 18:52:11 +0200

    Update funkcijos.h

--- header/funkcijos.h
+++ header/funkcijos.h
@@ -7,6 +7,7 @@
 #include <list>
 #include <string>
 #include <fstream>
+#include <sstream>
 #include <random>
 #include <algorithm>
 #include <numeric>
@@ -23,7 +24,7 @@ void generuotiPazymius(Studentas& studentas, int namuDarbuKiekis);
 void nuskaitytiStudentus(const std::string& failoPavadinimas, std::vector<Studentas>& studentai);
 void nuskaitytiStudentus(const std::string& failoPavadinimas, std::list<Studentas>& studentai);
 
-void strategija1(std::vector<Studentas>& studentai, bool pagalVidurki, std::vector<Studentas>& vargsiukai, std::vector<Studentas>& kietekai);
+void strategija1( std::vector<Studentas>& studentai, bool pagalVidurki, std::vector<Studentas>& vargsiukai, std::vector<Studentas>& kietekai);
 void strategija1(std::list<Studentas>& studentai, bool pagalVidurki, std::list<Studentas>& vargsiukai, std::list<Studentas>& kietekai);
 
 void strategija2(std::vector<Studentas>& studentai, bool pagalVidurki, std::vector<Studentas>& vargsiukai, std::vector<Studentas>& kietekai);
@@ -32,17 +33,17 @@ void strategija2(std::list<Studentas>& studentai, bool pagalVidurki, std::list<S
 void strategija3(std::vector<Studentas>& studentai, bool pagalVidurki, std::vector<Studentas>& vargsiukai, std::vector<Studentas>& kietekai);
 void strategija3(std::list<Studentas>& studentai, bool pagalVidurki, std::list<Studentas>& vargsiukai, std::list<Studentas>& kietekai);
 
-void isaugotiStudentuGrupe(const std::vector<Studentas>& studentai, const std::string& failoPavadinimas, bool pagalVidurki);
-void isaugotiStudentuGrupe(const std::list<Studentas>& studentai, const std::string& failoPavadinimas, bool pagalVidurki);
+void isaugotiStudentuGrupe(const std::vector<Studentas>& studentai, const std::string& failoPavadinimas);
+void isaugotiStudentuGrupe(const std::list<Studentas>& studentai, const std::string& failoPavadinimas);
 
 void ivestiStudentuDuomenis(std::vector<Studentas>& studentaiVektorius);
 void ivestiStudentuDuomenis(std::list<Studentas>& studentaiSarasas);
 
-void testKonteinerius(const std::vector<Studentas>& studentaiVektorius, bool pagalVidurki,
+void testKonteinerius(std::vector<Studentas>& studentaiVektorius, bool pagalVidurki,
     double& totalPartitionTimeVec, double& totalNuskriaustukaiSaveTimeVec, double& totalKietiakaiSaveTimeVec,
     void (*strategija)(std::vector<Studentas>&, bool, std::vector<Studentas>&, std::vector<Studentas>&));
 
-void testKonteinerius(const std::list<Studentas>& studentaiLista, bool pagalVidurki,
+void testKonteinerius(const std::list<Studentas>& studentaiList, bool pagalVidurki,
     double& totalPartitionTimeList, double& totalNuskriaustukaiSaveTimeList, double& totalKietiakaiSaveTimeList,
     void (*strategija)(std::list<Studentas>&, bool, std::list<Studentas>&, std::list<Studentas>&));
 
