eee99eb9e6646255baaaa8ec0f4cb1ec250e3ec2
karina-stur <sturonaite.k@gmail.com>
2024-12-13 00:01:26 +0200

    Update funkcijos.cpp

--- src/funkcijos.cpp
+++ src/funkcijos.cpp
@@ -1,6 +1,15 @@
 #include "funkcijos.h"
 #include "studentas.h"
 
+/**
+ * @brief Funkcija, skirta ivesti sveikaji skaiciu su patikra, kad jis butu teigiamas.
+ *
+ * Si funkcija nuolat praso vartotojo ivesti teigiamaji sveikaji skaiciu. Jeigu ivestas
+ * skaicius yra klaidingas arba neigiamas, bus rodomas klaidos pranesimas ir vartotojas
+ * bus paprasytas ivesti skaiciu is naujo, kol bus ivestas teisingas teigiamas skaicius.
+ *
+ * @return int Teisingai ivestas sveikasis skaicius.
+ */
 int readInteger() {
     int value;
     while (true) {
@@ -18,6 +27,15 @@ int readInteger() {
     return value;
 }
 
+/**
+ * @brief Funkcija, skirta generuoti studentu failus su atsitiktiniais duomenimis.
+ *
+ * Si funkcija sukuria kelis studentu duomenu failus su atsitiktiniais pazymiais
+ * uz namu darbus ir egzaminus. Failai kuriami pagal is anksto nustatyta irasu kieki
+ * (1000, 10000 ir t.t.). Jeigu failas jau egzistuoja, jis bus praleistas.
+ *
+ * @return void
+ */
 void generuotiStudentuFailus() {
     std::vector<int> record_counts = { 1000, 10000, 100000, 1000000, 10000000 };
     std::random_device rd;
@@ -51,6 +69,16 @@ void generuotiStudentuFailus() {
     }
 }
 
+/**
+ * @brief Funkcija, skirta generuoti atsitiktini pazymi.
+ *
+ * Funkcija sugeneruoja atsitiktini pazymi pagal nurodytus minimalu ir maksimalu diapazonus.
+ *
+ * @param min Minimalus galimas pazymys (numatytoji reiksme: 1).
+ * @param max Maksimalus galimas pazymys (numatytoji reiksme: 10).
+ *
+ * @return int Atsitiktinai sugeneruotas pazymys.
+ */
 int generuotiAtsitiktiniPazymi(int min = 1, int max = 10) {
     static std::random_device rd;
     static std::mt19937 mt(rd());
@@ -58,6 +86,16 @@ int generuotiAtsitiktiniPazymi(int min = 1, int max = 10) {
     return dist(mt);
 }
 
+/**
+ * @brief Funkcija, skirta generuoti studento pazymius.
+ *
+ * Si funkcija sugeneruoja atsitiktinius pazymius studentui uz nurodyta namu darbu kieki.
+ *
+ * @param studentas Studentas, kuriam generuojami pazymiai.
+ * @param namuDarbuKiekis Namu darbu kieki, pagal kuri bus sugeneruoti pazymiai.
+ *
+ * @return void
+ */
 void generuotiPazymius(Studentas& studentas, int namuDarbuKiekis) {
     studentas.setNamuDarbai({});
 
@@ -66,11 +104,30 @@ void generuotiPazymius(Studentas& studentas, int namuDarbuKiekis) {
     }
 }
 
+/**
+ * @brief Funkcija, skirta skaiciuoti galutini studento vidurki.
+ *
+ * Si funkcija apskaiciuoja galutini studento vidurki, atsizvelgiant i visus namu darbus ir egzamina.
+ *
+ * @param namuDarbai Studentui uzduoti namu darbu pazymiai.
+ * @param egzaminas Egzamino pazymys.
+ *
+ * @return double Galutinis vidurkis.
+ */
 double skaiciuotiGalutiniVidurki(const std::vector<int>& namuDarbai, int egzaminas) {
     double total = std::accumulate(namuDarbai.begin(), namuDarbai.end(), 0);
     return (total + egzaminas) / (namuDarbai.size() + 1);
 }
 
+/**
+ * @brief Funkcija, skirta skaiciuoti galutine mediana.
+ *
+ * Si funkcija apskaiciuoja galutine studento pazymiu mediana is visu namu darbu.
+ *
+ * @param namuDarbai Studentui uzduoti namu darbu pazymiai.
+ *
+ * @return double Galutine mediana.
+ */
 double skaiciuotiGalutiniMediana(const std::vector<int>& namuDarbai) {
     std::vector<int> sortedNamuDarbai = namuDarbai;
     std::sort(sortedNamuDarbai.begin(), sortedNamuDarbai.end());
@@ -83,11 +140,30 @@ double skaiciuotiGalutiniMediana(const std::vector<int>& namuDarbai) {
     }
 }
 
+/**
+ * @brief Funkcija, skirta skaiciuoti galutini studento vidurki su lista.
+ *
+ * Si funkcija apskaiciuoja galutini studento vidurki, atsizvelgiant i visus namu darbus ir egzamina.
+ *
+ * @param namuDarbai Studentui uzduoti namu darbu pazymiai.
+ * @param egzaminas Egzamino pazymys.
+ *
+ * @return double Galutinis vidurkis.
+ */
 double skaiciuotiGalutiniVidurki(const std::list<int>& namuDarbai, int egzaminas) {
     double total = std::accumulate(namuDarbai.begin(), namuDarbai.end(), 0);
     return (total + egzaminas) / (namuDarbai.size() + 1);
 }
 
+/**
+ * @brief Funkcija, skirta skaiciuoti galutine mediana su lista.
+ *
+ * Si funkcija apskaiciuoja galutine studento pazymiu mediana is visu namu darbu.
+ *
+ * @param namuDarbai Studentui uzduoti namu darbu pazymiai.
+ *
+ * @return double Galutine mediana.
+ */
 double skaiciuotiGalutiniMediana(const std::list<int>& namuDarbai) {
     std::vector<int> sortedNamuDarbai(namuDarbai.begin(), namuDarbai.end());
     std::sort(sortedNamuDarbai.begin(), sortedNamuDarbai.end());
@@ -100,6 +176,15 @@ double skaiciuotiGalutiniMediana(const std::list<int>& namuDarbai) {
     }
 }
 
+/**
+ * @brief Funkcija, skirta ivesti studentu duomenis i vektoriu.
+ *
+ * Si funkcija leidzia vartotojui ivesti studentu duomenis ir saugoti juos vektoriuje.
+ *
+ * @param studentaiVektorius Vektorius, kuriame bus saugomi studentai.
+ *
+ * @return void
+ */
 void ivestiStudentuDuomenis(std::vector<Studentas>& studentaiVektorius) {
     int kiekMokiniu;
     std::cout << "Kiek studentu norite ivesti? ";
@@ -107,7 +192,7 @@ void ivestiStudentuDuomenis(std::vector<Studentas>& studentaiVektorius) {
 
     for (int i = 0; i < kiekMokiniu; ++i) {
         Studentas studentas;
-        std::cin >> studentas; 
+        std::cin >> studentas;
         studentaiVektorius.push_back(studentas);
 
         std::cout << "Studentas " << studentas.getVardas() << " " << studentas.getPavarde()
@@ -115,6 +200,15 @@ void ivestiStudentuDuomenis(std::vector<Studentas>& studentaiVektorius) {
     }
 }
 
+/**
+ * @brief Funkcija, skirta ivesti studentu duomenis i sarasa.
+ *
+ * Si funkcija leidzia vartotojui ivesti studentu duomenis ir saugoti juos sarase.
+ *
+ * @param studentaiSarasas Sarasas, kuriame bus saugomi studentai.
+ *
+ * @return void
+ */
 void ivestiStudentuDuomenis(std::list<Studentas>& studentaiSarasas) {
     int kiekMokiniu;
     std::cout << "Kiek studentu norite ivesti? ";
@@ -130,6 +224,15 @@ void ivestiStudentuDuomenis(std::list<Studentas>& studentaiSarasas) {
     }
 }
 
+/**
+ * @brief Nuskaityti studentus is failo ir issaugoti i vektoriu.
+ *
+ * Si funkcija atidaro faila pagal pateikta pavadinima, nuskaityti studento duomenis
+ * (vardas, pavarde, namu darbai ir egzaminas) ir issaugo juos i vektoriu.
+ *
+ * @param failoPavadinimas Failo, is kurio bus nuskaityti studentu duomenys, pavadinimas.
+ * @param studentai Vektorius, kuriame bus issaugoti studentai.
+ */
 void nuskaitytiStudentus(const std::string& failoPavadinimas, std::vector<Studentas>& studentai) {
     std::ifstream failas(failoPavadinimas);
     if (!failas.is_open()) {
@@ -161,6 +264,15 @@ void nuskaitytiStudentus(const std::string& failoPavadinimas, std::vector<Studen
     }
 }
 
+/**
+ * @brief Nuskaityti studentus is failo ir issaugoti i sarasa.
+ *
+ * Si funkcija atidaro faila pagal pateikta pavadinima, nuskaityti studento duomenis
+ * (vardas, pavarde, namu darbai ir egzaminas) ir issaugo juos i sarasa.
+ *
+ * @param failoPavadinimas Failo, is kurio bus nuskaityti studentu duomenys, pavadinimas.
+ * @param studentai Sarasas, kuriame bus issaugoti studentai.
+ */
 void nuskaitytiStudentus(const std::string& failoPavadinimas, std::list<Studentas>& studentai) {
     std::ifstream failas(failoPavadinimas);
     if (!failas.is_open()) {
@@ -191,6 +303,14 @@ void nuskaitytiStudentus(const std::string& failoPavadinimas, std::list<Studenta
     }
 }
 
+/**
+ * @brief Iraso studentu grupe i ekrana.
+ *
+ * Si funkcija parodo studentu duomenis (vardas, pavarde, galutinis vidurkis ir galutinis mediana)
+ * ekrane is pateikto studentu vektoriaus.
+ *
+ * @param studentai Vektorius, kuriame yra studentai, kuriu duomenys bus atvaizduoti.
+ */
 void isaugotiStudentuGrupe(const std::vector<Studentas>& studentai) {
     if (studentai.empty()) {
         std::cerr << "Studentu nera, nera ka irasyti!" << std::endl;
@@ -208,6 +328,14 @@ void isaugotiStudentuGrupe(const std::vector<Studentas>& studentai) {
     }
 }
 
+/**
+ * @brief Iraso studentu grupe i ekrana.
+ *
+ * Si funkcija parodo studentu duomenis (vardas, pavarde, galutinis vidurkis ir galutinis mediana)
+ * ekrane is pateikto studentu saraso.
+ *
+ * @param studentai Sarasas, kuriame yra studentai, kuriu duomenys bus atvaizduoti.
+ */
 void isaugotiStudentuGrupe(const std::list<Studentas>& studentai) {
     if (studentai.empty()) {
         std::cerr << "Studentu nera, nera ka irasyti!" << std::endl;
@@ -225,6 +353,18 @@ void isaugotiStudentuGrupe(const std::list<Studentas>& studentai) {
     }
 }
 
+/**
+ * @brief Strategija 1: Studentu suskirstymas i vargsiukus ir kietekus naudojant viena kontineri.
+ *
+ * Si funkcija suskirsto studentus i dvi grupes - vargsiukus (studentai su galutiniu rezultatu
+ * maziau nei 5) ir kietekus (studentai su galutiniu rezultatu 5 ir daugiau) pagal vidurki arba
+ * mediana.
+ *
+ * @param studentai Vektorius studentu, kurie bus suskirstyti.
+ * @param pagalVidurki Jei true, naudojamas galutinis vidurkis, jei false - galutinis mediana.
+ * @param vargsiukai Vektorius, i kuri bus irasomi vargsiukai.
+ * @param kietekai Vektorius, i kuri bus irasomi kietekai.
+ */
 void strategija1(std::vector<Studentas>& studentai, bool pagalVidurki,
     std::vector<Studentas>& vargsiukai, std::vector<Studentas>& kietekai) {
     std::vector<std::pair<Studentas, double>> studentScores;
@@ -252,6 +392,18 @@ void strategija1(std::vector<Studentas>& studentai, bool pagalVidurki,
     }
 }
 
+/**
+ * @brief Strategija 1: Studentu suskirstymas i vargsiukus ir kietekus naudojant viena konteineri.
+ *
+ * Si funkcija suskirsto studentus i dvi grupes - vargsiukus (studentai su galutiniu rezultatu
+ * maziau nei 5) ir kietekus (studentai su galutiniu rezultatu 5 ir daugiau) pagal vidurki arba
+ * mediana. Naudoja sarasa vietoj vektoriaus.
+ *
+ * @param studentai Sarasas studentu, kurie bus suskirstyti.
+ * @param pagalVidurki Jei true, naudojamas galutinis vidurkis, jei false - galutinis mediana.
+ * @param vargsiukai Sarasas, i kuri bus irasomi vargsiukai.
+ * @param kietekai Sarasas, i kuri bus irasomi kietekai.
+ */
 void strategija1(std::list<Studentas>& studentai, bool pagalVidurki, std::list<Studentas>& vargsiukai, std::list<Studentas>& kietekai) {
     std::list<Studentas> listCopy = studentai;
 
@@ -273,6 +425,19 @@ void strategija1(std::list<Studentas>& studentai, bool pagalVidurki, std::list<S
     }
 }
 
+
+/**
+ * @brief Strategija 2: Studentu suskirstymas i vargsiukus ir kietekus naudojant 2 atskirus konteinerius.
+ *
+ * Si funkcija naudoja vektoriu ir perkelia studentus i atskiras grupes (vargsiukai ir kietekai),
+ * remiantis galutiniu rezultatu, kuris apskaiciuojamas pagal vidurki arba medianna. Studentai su
+ * galutiniu rezultatu maziau nei 5 yra perkelti i vargsiuku grupe, o visi kiti - i kieteku grupe.
+ *
+ * @param studentai Vektorius studentu, kuriuos reikia suskirstyti.
+ * @param pagalVidurki Jei true, naudojamas galutinis vidurkis, jei false - galutinis mediana.
+ * @param vargsiukai Vektorius, kuriame bus issaugoti vargsiukai.
+ * @param kietekai Vektorius, kuriame bus issaugoti kietekai.
+ */
 void strategija2(std::vector<Studentas>& studentai, bool pagalVidurki, std::vector<Studentas>& vargsiukai, std::vector<Studentas>& kietekai) {
     vargsiukai.clear();
     kietekai.clear();
@@ -298,6 +463,18 @@ void strategija2(std::vector<Studentas>& studentai, bool pagalVidurki, std::vect
     studentai.resize(writeIndex);
 }
 
+/**
+ * @brief Studentu suskirstymas i vargsiukus ir kietekus naudojant 2 atskirus konteinerius.
+ *
+ * Si funkcija naudoja sarasa ir perkelia studentus i atskiras grupes (vargsiukai ir kietekai),
+ * remiantis galutiniu rezultatu, kuris apskaiciuojamas pagal vidurki arba medianna. Studentai su
+ * galutiniu rezultatu maziau nei 5 yra perkelti i vargsiuku grupe, o visi kiti - i kieteku grupe.
+ *
+ * @param studentai Sarasas studentu, kuriuos reikia suskirstyti.
+ * @param pagalVidurki Jei true, naudojamas galutinis vidurkis, jei false - galutinis mediana.
+ * @param vargsiukai Sarasas, kuriame bus issaugoti vargsiukai.
+ * @param kietekai Sarasas, kuriame bus issaugoti kietekai.
+ */
 void strategija2(std::list<Studentas>& studentai, bool pagalVidurki, std::list<Studentas>& vargsiukai, std::list<Studentas>& kietekai) {
     vargsiukai.clear();
     kietekai.clear();
@@ -308,7 +485,7 @@ void strategija2(std::list<Studentas>& studentai, bool pagalVidurki, std::list<S
 
         if (finalScore < 5.0) {
             vargsiukai.push_back(std::move(*it));
-            it = studentai.erase(it); 
+            it = studentai.erase(it);
         }
         else {
             kietekai.push_back(std::move(*it));
@@ -317,6 +494,18 @@ void strategija2(std::list<Studentas>& studentai, bool pagalVidurki, std::list<S
     }
 }
 
+/**
+ * @brief Strategija 3: Studentu suskirstymas i vargsiukus ir kietekus naudojant geriausia strategija.
+ *
+ * Si funkcija naudoja vektoriu ir padalina studentus i vargsiukus ir kietekus, naudodama
+ * std::partition funkcija. Studentai, kuriu galutinis rezultatas yra maziau nei 5, bus perkelti
+ * i vargsiuku grupe, o visi kiti - i kieteku grupe.
+ *
+ * @param studentai Vektorius studentu, kuriuos reikia suskirstyti.
+ * @param pagalVidurki Jei true, naudojamas galutinis vidurkis, jei false - galutinis mediana.
+ * @param vargsiukai Vektorius, kuriame bus issaugoti vargsiukai.
+ * @param kietekai Vektorius, kuriame bus issaugoti kietekai.
+ */
 void strategija3(std::vector<Studentas>& studentai, bool pagalVidurki, std::vector<Studentas>& vargsiukai, std::vector<Studentas>& kietekai) {
     vargsiukai.clear();
     kietekai.clear();
@@ -333,6 +522,18 @@ void strategija3(std::vector<Studentas>& studentai, bool pagalVidurki, std::vect
     studentai.resize(std::distance(studentai.begin(), partitionIt));
 }
 
+/**
+ * @brief Strategija 3: Studentu suskirstymas i vargsiukus ir kietekus naudojant geriausia strategija.
+ *
+ * Si funkcija naudoja sarasa ir padalina studentus i vargsiukus ir kietekus. Studentai,
+ * kuriu galutinis rezultatas yra maziau nei 5, bus perkelti i vargsiuku grupe, o visi kiti -
+ * i kieteku grupe.
+ *
+ * @param studentai Sarasas studentu, kuriuos reikia suskirstyti.
+ * @param pagalVidurki Jei true, naudojamas galutinis vidurkis, jei false - galutinis mediana.
+ * @param vargsiukai Sarasas, kuriame bus issaugoti vargsiukai.
+ * @param kietekai Sarasas, kuriame bus issaugoti kietekai.
+ */
 void strategija3(std::list<Studentas>& studentai, bool pagalVidurki, std::list<Studentas>& vargsiukai, std::list<Studentas>& kietekai) {
     vargsiukai.clear();
     kietekai.clear();
@@ -349,6 +550,21 @@ void strategija3(std::list<Studentas>& studentai, bool pagalVidurki, std::list<S
         }
     }
 }
+
+/**
+ * @brief Testuoja konteinerius su std::vector<Studentas>.
+ *
+ * Funkcija testuoja strategija su std::vector<Studentas> konteineriu, suskirstydama studentus i vargsiukus ir kietekus.
+ * Atlikus strategija, rezultatai gali buti isvedami i ekrana arba issaugomi faile.
+ * Matuojamas laikas, reikalingas grupei suskirstyti ir rezultatams issaugoti.
+ *
+ * @param studentaiVector Vektorius studentu, kurie bus testuojami.
+ * @param pagalVidurki Kintamasis, nurodantis, ar naudoti vidurki (true) ar mediana (false) kaip galutini rezultata.
+ * @param totalPartitionTimeVec Busimas kintamasis, kuriame bus saugomas laikas, per kuri buvo suskirstyti studentai.
+ * @param totalNuskriaustukaiSaveTimeVec Busimas kintamasis, kuriame bus saugomas laikas, per kuri buvo issaugoti nuskriaustukai.
+ * @param totalKietiakaiSaveTimeVec Busimas kintamasis, kuriame bus saugomas laikas, per kuri buvo issaugoti kietekai.
+ * @param strategija Funkcija, kuri atlieka studentu suskirstyma pagal tam tikra strategija.
+ */
 
 void testKonteinerius(const std::vector<Studentas>& studentaiVector, bool pagalVidurki,
     double& totalPartitionTimeVec, double& totalNuskriaustukaiSaveTimeVec, double& totalKietiakaiSaveTimeVec,
@@ -448,6 +664,21 @@ void testKonteinerius(const std::vector<Studentas>& studentaiVector, bool pagalV
     std::cout << "Bendrai uztruko: " << totalTimeVec << " sekundziu.\n";
 }
 
+/**
+ * @brief Testuoja konteinerius su std::list<Studentas>.
+ *
+ * Funkcija testuoja strategija su std::list<Studentas> konteineriu, suskirstydama studentus i vargsiukus ir kietekus.
+ * Atlikus strategija, rezultatai gali buti isvedami i ekrana arba issaugomi faile.
+ * Matuojamas laikas, reikalingas grupei suskirstyti ir rezultatams issaugoti.
+ *
+ * @param studentaiList Sarasas studentu, kurie bus testuojami.
+ * @param pagalVidurki Kintamasis, nurodantis, ar naudoti vidurki (true) ar mediana (false) kaip galutini rezultata.
+ * @param totalPartitionTimeList Busimas kintamasis, kuriame bus saugomas laikas, per kuri buvo suskirstyti studentai.
+ * @param totalNuskriaustukaiSaveTimeList Busimas kintamasis, kuriame bus saugomas laikas, per kuri buvo issaugoti nuskriaustukai.
+ * @param totalKietiakaiSaveTimeList Busimas kintamasis, kuriame bus saugomas laikas, per kuri buvo issaugoti kietekai.
+ * @param strategija Funkcija, kuri atlieka studentu suskirstyma pagal tam tikra strategija.
+ */
+
 void testKonteinerius(const std::list<Studentas>& studentaiList, bool pagalVidurki,
     double& totalPartitionTimeList, double& totalNuskriaustukaiSaveTimeList, double& totalKietiakaiSaveTimeList,
     void (*strategija)(std::list<Studentas>&, bool, std::list<Studentas>&, std::list<Studentas>&)) {
@@ -545,6 +776,16 @@ void testKonteinerius(const std::list<Studentas>& studentaiList, bool pagalVidur
     std::cout << "Kietekai isaugoti per: " << saveKietekaiTime << " sekundziu.\n";
     std::cout << "Bendrai uztruko: " << totalTimeList << " sekundziu.\n";
 }
+
+/**
+ * @brief Demonstracija su trimis studento objektais.
+ *
+ * Funkcija demonstruoja, kaip veikia priskyrimo operatorius ir kopijavimo konstruktorius su Studentas objektais.
+ *
+ * @note Funkcija sukuria studento objekta `a`, tada priskiria ji i objekta `b` ir sukuria objekta `c` naudodama kopijavimo konstruktoriumi.
+ *
+ * @param none
+ */
 
 void trysMetodai() {
     Studentas a;
